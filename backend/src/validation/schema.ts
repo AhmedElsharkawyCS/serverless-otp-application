59ee1a0db9072b1db68f57d423960c338641dbe4
AhmedElsharkawyCS <ahmedelsharkawy.fcih@gmail.com>
2021-05-01 20:40:26 +0200

    update backend index

--- backend/src/validation/schema.ts
+++ backend/src/validation/schema.ts
@@ -1,11 +1,11 @@
 import Joi from "joi";
 const emailRgx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const otpRgx = /^[a-zA-Z0-9]{10,10}$/;
+const otpRgx = /^[a-zA-Z0-9]{8,8}$/;
 
 export const sendOTPSchemaValidation = Joi.object({
   email: Joi.string().regex(emailRgx).required().error(new Error("Invalid email, it should be like this format example@domain.com")),
 });
 
 export const verifyOTPSchemaValidation = Joi.object({
-  otpCode: Joi.string().regex(otpRgx).required().error(new Error("Invalid OTP code, it should be equal 10 digits.")),
+  otpCode: Joi.string().regex(otpRgx).required().error(new Error("Invalid OTP code, it should be equal 8 digits.")),
 });
